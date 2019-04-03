589c68623fc2f65215efa781680290775c7b9e31
victorN <victornguyen082@gmail.com>
2019-04-03 16:21:20 -0400

    this.js completion

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. If this is within the Global Scope, it is referring to the window.
+* 2. Whenever this is followed with a dot, it is referring to the object before the dot. Implicit Binding.
+* 3. You must create a new binding whenever you're using a constructor function.
+* 4. Explicit binding is when you call() or apply(), this is explicit.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,52 @@
 // Principle 1
 
 // code example for Window Binding
+function hello(helloWorld) {
+    return `Hello ${this.name}!`;
+}
 
 // Principle 2
 
 // code example for Implicit Binding
+const pokemon = {
+    type: "Fire",
+    name: "Charmander",
+    pokedexNumber: 4,
+    aboutPokemon: function() {
+        return `${this.name} is a ${this.type} and is no #${this.pokedexNumber}.`;
+    }
+}
+console.log(pokemon.aboutPokemon());
+
 
 // Principle 3
 
 // code example for New Binding
+function PokemonAbilitys() {
+    this.go = 'Go'
+    this.attack = 'attack!',
+    this.fight = function() {
+        console.log(`${this.go}, ${this.attack}`);
+        console.log(this);
+    }
+
+}
+const mewtwo = new PokemonAbilitys('Mewtwo');
+const abra = new PokemonAbilitys('Abra');
+mewtwo.fight();
+abra.fight();
+
 
 // Principle 4
 
 // code example for Explicit Binding
+const squirtle = {
+    name: "Squirtle",
+    type: "Water"
+}
+function infoSquirtle(move1, move2, move3, move4) {
+    return `This is ${this.name} and knows ${move1}, ${move2}, ${move3}, ${move4}`;
+}
+const move = ["Tackle", "Bubble", "Water Gun", "Tail Whip"];
+
+console.log(infoSquirtle.apply(squirtle, move));
